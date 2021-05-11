33ddc277418208e3aa1260ffac4892a0e5241c51
VjekoslavLalic <VjekoslavLalic@users.noreply.github.com>
2021-05-11 14:23:39 +0200

    Navbar i mali navbar

---
+++ src/views/Piva.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="piva">
+    <div class="pivamenu">
+      <a>Sva piva</a> | <a>Stalna piva</a>| <a>Sezonska piva</a>|
+      <a>Hladno pivo</a>
+    </div>
+  </div>
+</template>
+<script>
+</script>
+<style>
+</style>
+
---
+++ src/views/Pivnice.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <p>Ovo je ruta pivnica</p>
+  </div>
+</template>
+
+<script>
+</script>
